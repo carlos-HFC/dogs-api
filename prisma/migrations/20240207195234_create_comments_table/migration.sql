e18e8e13900f9cfa7b43b8ef074f246ab65fbb8a
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-07 16:52:41 -0300

    feat: add comments table

---
+++ prisma/migrations/20240207195234_create_comments_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "photoId" TEXT NOT NULL,
+    "date" DATETIME NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Comment_photoId_fkey" FOREIGN KEY ("photoId") REFERENCES "Photo" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
