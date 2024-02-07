226120cc6ebf1fd818c8e9a8243fb8bec393307f
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-07 17:02:49 -0300

    feat: add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+
+import { envSchema } from "./infra/env/env";
+import { EnvModule } from "./infra/env/env.module";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true
+    })
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    EnvModule
+  ],
 })
 export class AppModule {}
