9507ee38f0371fdd9a946c6b6d1265fe67ee43f7
Carlos Faustino <chfcchfc96@gmail.com>
2024-02-08 12:46:37 -0300

    feat: add database in memory

---
+++ test/repositories/in-memory-users.repository.ts
@@ -0,0 +1,38 @@
+import { User } from "@/domain/user/application/entities/user";
+import { UserRepository } from "@/domain/user/application/repositories/user.repository";
+
+export class InMemoryUsersRepository implements UserRepository {
+  public users: User[] = [];
+
+  async findById(id: string): Promise<User | null> {
+    const user = this.users.find(item => item.id.toString() === id);
+
+    if (!user) return null;
+
+    return user;
+  }
+
+  async findByUsername(username: string): Promise<User | null> {
+    const user = this.users.find(item => item.username === username);
+
+    if (!user) return null;
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.users.find(item => item.email === email);
+
+    if (!user) return null;
+
+    return user;
+  }
+
+  async create(user: User): Promise<void> {
+    this.users.push(user);
+  }
+
+  async save(user: User): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
